e62af94fa289fed009ee0d179b7c3f0d3d5d441c
AlgonzaDJun <algonsadewangga@gmail.com>
2023-11-25 12:00:27 +0700

    add user

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  fullname: String,
+  email: String,
+  no_hp: String,
+  password: String,
+  mood: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "hasilQuiz",
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
