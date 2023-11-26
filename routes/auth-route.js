10b1a2bf341b6633868f8efceff8e70529137a83
Muhammad Saman <samanmuhammad077@gmail.com>
2023-11-26 09:58:23 +0800

    finishing feature sending otp when create new account

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { login, register } = require("../controllers/auth.controller")
+const { login, register } = require("../controllers/auth.controller");
+const { sendOTP } = require("../controllers/otp.controller");
 const route = express.Router();
 
 route.post("/login", login);
 route.post("/register", register);
+route.post("/send-otp", sendOTP);
 
 module.exports = {
-    authRoute: route,
-}
+  authRoute: route,
+};
