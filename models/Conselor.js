e6b8a3887e140df6c28d00a02ac0ef68e0239cb8
Melati11 <2110631250049@student.unsika.ac.id>
2023-12-01 09:01:25 +0700

    update model conselor

---
+++ models/Conselor.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const rateSchema = new Schema(
+  {
+    user_id: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    rate: Number,
+    comment: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const ConselorSchema = new Schema(
+  {
+    user_id: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    spesialisasi: String,
+    deskripsi: String,
+    status: String,
+    rate: [rateSchema],
+    schedule: [
+      {
+        day: String,
+        time: String,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Conselor = mongoose.model("Conselor", ConselorSchema);
+
+module.exports = Conselor;
