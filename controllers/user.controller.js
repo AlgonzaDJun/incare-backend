dbb09c4d00f71a0fcc18396c80b9f3d79aa52118
Muhammad Saman <samanmuhammad077@gmail.com>
2023-12-05 19:49:45 +0800

    modified:   controllers/user.controller.js modified:   models/user.js modified:   routes/user-route.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,7 +4,8 @@ const { hasilQuiz } = require("../models/mini-quiz");
 const jwt = require("jsonwebtoken");
 const { internalServer } = require("../utils/error");
 const Conselor = require("../models/Conselor");
-
+const imagekit = require("../utils/imageKit");
+const path = require("path");
 module.exports = {
   getAllUser: async (req, res) => {
     const users = await User.find();
@@ -26,20 +27,52 @@ module.exports = {
         const conselor = await Conselor.findOne(
           { user_id: userId },
           "spesialisasi rate schedule price"
-        ).populate("user_id", "username fullname bio");
+        ).populate("user_id", "username fullname bio image_url");
         return res.status(200).json({
           status: "OK",
           message: "Get Profile User Successfully",
           data: conselor,
         });
       }
-      const user = await User.findById(userId, "username fullname bio");
+      const user = await User.findById(
+        userId,
+        "username fullname bio image_url"
+      );
       return res.status(200).json({
         status: "OK",
         message: "Get Profile User Successfully",
         data: user,
       });
     } catch (error) {
+      return internalServer(res);
+    }
+  },
+  editProfile: async (req, res) => {
+    const header = req.headers.authorization;
+    const token = header.split(" ")[1];
+    const decoded = jwt.verify(token, "treasure");
+    const userId = decoded.id;
+    const { fullname, bio } = req.body;
+    if (!req.file) {
+      return res.status(400).send("No file uploaded.");
+    }
+    try {
+      const uploadResponse = await imagekit.upload({
+        file: req.file.buffer.toString("base64"),
+        fileName: "profile" + path.extname(req.file.originalname),
+      });
+      const imageUrl = uploadResponse.name;
+      await User.findByIdAndUpdate(userId, {
+        fullname,
+        bio,
+        image_url: imageUrl,
+      });
+      return res.status(200).json({
+        status: "OK",
+        message: "Update Profile Successfully",
+      });
+    } catch (error) {
+      console.log(error.message);
       return internalServer(res);
     }
   },
